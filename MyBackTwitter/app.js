95d5a737e2e990c86d4c68a293359adcb97fdbd3
Sebastián Díaz Gaona <sediazga@poligran.edu.co>
2020-03-22 19:53:25 -0500

    conection db

--- MyBackTwitter/app.js
+++ MyBackTwitter/app.js
@@ -8,6 +8,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const dbManager = require('./database/db.mannager');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +18,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/**
+ * Testing the connection to the database and recreate the models if the tables doesn´t exists  
+ */
+dbManager.sequelizeConnection.authenticate()
+  .then(() => {
+    console.log('****Connection has been established successfully.****');
+    // recreate the models if the tables doesn´t exists
+    dbManager.sequelizeConnection.sync().then(() => {
+        console.log("Database Synced");
+      });
+
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 module.exports = app;
