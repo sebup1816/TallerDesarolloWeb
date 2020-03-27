2f56946ca6511d65c19dad39fc2f74ffc08415d2
Jimmy Giovanny Forero Forero <jiforero2@poligran.edu.co>
2020-03-27 00:30:46 -0500

    falto

--- MyBackTwitter/database/db.mannager.js
+++ MyBackTwitter/database/db.mannager.js
@@ -6,19 +6,24 @@ const sequelizeConnection = require('./db.connection');
 //IMPORT MODELS
 const UserModel = require("../models/user.model");
 const PostModel = require("../models/post.model");
+const MessageModel = require ("../models/message.model");
 
 //INITIALIZE MODELS
 const User = UserModel (sequelizeConnection, Sequelize);
 const Post = PostModel (sequelizeConnection, Sequelize);
+const Message = MessageModel (sequelizeConnection, Sequelize);
 
 //CREATE RELATIONS BETWEEN MODELS
 User.hasMany(Post, { foreignKey: 'idPost', sourceKey: 'idUser' });
 Post.belongsTo(User, { foreignKey: 'idUser', sourceKey: 'idPost' });
+User.hasMany(Message, { foreignKey: 'idMessage', sourceKey: 'idUser' });
+Message.belongsTo(User, { foreignKey: 'idUser', sourceKey: 'idMessage' });
 
 //GROUP MODELS
 const models = {
   User: User,
   Post: Post,
+  Message: Message,
 };
 
 
